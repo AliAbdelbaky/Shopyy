b22dcdc4a0578eaf8e7f0bf7f526e85e27d4664e
@ali_abdebaqy <ali.abdelbaqy@grand-community.com>
2023-05-17 03:13:54 +0300

    opt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,11 @@ export default defineNuxtConfig({
     AUTH_SECRET: process.env.AUTH_SECRET,
     // Public keys that are exposed to the client
     apiBase: process.env.BASE_URL || "/api",
+    auth:{
+      computed:{
+        origin: process.env.ORIGIN
+      }
+    }
   },
   app: {
     head: {
